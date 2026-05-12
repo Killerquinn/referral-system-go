f0fef67d5e2ece16a2822f6dbf955400ea16de2e
Killerquinn <xxxilluminatixxxm@gmail.com>
2026-05-12 09:30:31 +0300

    app

--- internal/features/user/handler.go
+++ internal/features/user/handler.go
@@ -1,1 +1,16 @@
 package user
+
+import "net/http"
+
+type UseCase interface {
+}
+
+type Service struct {
+	useCase UseCase
+}
+
+func NewHandler() *Service {
+	return &Service{useCase: useCase}
+}
+
+func (h *Service) CreateUser(w http.ResponseWriter, r *http.Request) {}
--- internal/infrastructure/http/rest/app/app.go
+++ internal/infrastructure/http/rest/app/app.go
@@ -3,17 +3,21 @@ package app
 import (
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/killerquinn/referral-system-go/internal/config"
+	"github.com/killerquinn/referral-system-go/internal/features/user"
 	"github.com/killerquinn/referral-system-go/internal/infrastructure/http/rest"
+	v1 "github.com/killerquinn/referral-system-go/internal/infrastructure/http/v1"
 	"go.uber.org/zap"
 )
 
 type App struct {
-	Server *rest.App
+	Router *chi.Router
 }
 
 func New(logger *zap.Logger, port int, tokenTTL time.Duration, cfg *config.Config) *App {
 	const op = "New"
+	restApp := rest.NewApp(logger, cfg)
 
 	return &App{}
 }
