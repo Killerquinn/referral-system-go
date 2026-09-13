6b0ca154c5808f3a17390e4e54d0976da1d37302
Killerquinn <xxxilluminatixxxm@gmail.com>
2026-09-13 19:12:40 +0300

    wip: added some 'empty' handlers

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # referral-system-go
+
+# B2B Referral Engine Microservice
+
+High-performance, scalable referral management service built with Go.
+
+## 🛠 Tech Stack
+
+* **Language:** Go (1.26.5+)
+* **Routing:** `go-chi/chi`
+* **Database:** PostgreSQL / `pgx`
+
+## 🏗 Architecture & Design Decisions
+
+* **Vertical Slice Architecture:** Domain features are grouped by business capabilities (e.g., `features/referrals`, `features/auth`), maximizing co-location and minimizing coupling.
+
+## 🚀 Getting Started
+
+### Prerequisites
+* Go 1.23+
+* PostgreSQL 16+
+
+### Still under development
+
+## License
+
+Distributed under the Apache 2.0 License. See `LICENSE` for more information.
+
+Copyright (c) 2026 Artem.
---
+++ internal/features/referral/http/handler.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type ReferralService interface {
+}
+
+type ReferralHandler struct {
+	rService ReferralService
+}
+
+func NewRefferalHandler(rs ReferralService) *ReferralHandler {
+	return &ReferralHandler{rService: rs}
+}
+
+func Register(r chi.Router, rs ReferralService) {
+	rh := NewRefferalHandler(rs)
+
+	r.Get("api/v1/referrals", rh.CurrentReferralList)
+	r.Post("api/v1/referrals/draw", rh.ContestBetweenReferrals)
+	r.Post("api/v1/referrals/becomereff", rh.BecomeSomeonesReferral)
+	r.Get("api/v1/refferer", rh.SeeWhoseReferralAlready)
+	r.Put("api/v1/changereferrer", rh.ChangeReferrer)
+}
+
+func (rh *ReferralHandler) CurrentReferralList(w http.ResponseWriter, r *http.Request) {
+	panic("implement me!")
+}
+
+func (rh *ReferralHandler) ContestBetweenReferrals(w http.ResponseWriter, r *http.Request) {
+	panic("implement me!")
+}
+
+func (rh *ReferralHandler) BecomeSomeonesReferral(w http.ResponseWriter, r *http.Request) {
+	panic("implement me!")
+}
+
+func (rh *ReferralHandler) SeeWhoseReferralAlready(w http.ResponseWriter, r *http.Request) {
+	panic("implement me!")
+}
+
+func (rh *ReferralHandler) ChangeReferrer(w http.ResponseWriter, r *http.Request) {
+	panic("implement me!")
+}
